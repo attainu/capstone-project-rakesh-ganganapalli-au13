3e69e4206e8df0cc666fa6350423a443c555eac9
Suraj Guptan <you@example.com>
2021-06-12 14:04:54 +0530

    backend1

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://suraj:user@123@cluster0.7ib5p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  JWT_SECRET:"sDfgtyUop45rTyu"
+};
---
+++ routes/auth.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const router = express.Router()
+const mongoose = require("mongoose")
+const User = mongoose.model("User")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const {JWT_SECRET} = require("../keys")
+
+router.get("/",(req,res)=>{
+    res.send("HI")
+})
+
+router.post("/signup", (req, res) => {
+  const { name, email, password, pic } = req.body;
+  if (!email || !password || !name) {
+    return res.status(422).json({ error: "please add all the fields" });
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res
+          .status(422)
+          .json({ error: "user already exists with that email" });
+      }
+      bcrypt.hash(password, 12).then((hashedpassword) => {
+        const user = new User({
+          email,
+          password: hashedpassword,
+          name,
+          pic,
+        });
+
+        user
+          .save()
+          .then((user) => {
+            // transporter.sendMail({
+            //     to:user.email,
+            //     from:"no-reply@insta.com",
+            //     subject:"signup success",
+            //     html:"<h1>welcome to instagram</h1>"
+            // })
+            res.json({ message: "saved successfully" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+
+module.exports = router
